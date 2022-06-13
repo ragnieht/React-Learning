305a01949f052fd48d2c243b630677f8ee425192
ragnieht <mrtheingar@gmail.com>
2022-06-14 01:10:06 +0800

    `1`

---
+++ my-react/src/App.js
@@ -0,0 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react/src/pages/NewMeetup.js
@@ -0,0 +1,5 @@
+function NewMeetup() {
+  return <div>NewMeetup page</div>;
+}
+
+export default NewMeetup;
